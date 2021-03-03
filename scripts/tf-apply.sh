16a6cade6bbbe8e356c0786deb1583afe2d20f37
Nelson Sanchez <anthony69snake@gmail.com>
2021-03-03 17:03:50 -0600

    am

--- scripts/tf-apply.sh
+++ scripts/tf-apply.sh
@@ -3,6 +3,5 @@
 set -e
 echo "Executing Terraform Apply on merged code"
 terraform init "$tf_init_cli_options"
-echo "THIS $PWD"
 terraform plan "$tf_plan_cli_options"
 # terraform apply $tf_apply_cli_options
