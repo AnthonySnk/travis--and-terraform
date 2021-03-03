fd9c4f3cd3fed74cf6076e4e96302ec7a70c1b3f
Nelson Sanchez <anthony69snake@gmail.com>
2021-03-03 16:38:43 -0600

    am

--- scripts/tf-plan.sh
+++ scripts/tf-plan.sh
@@ -2,6 +2,7 @@
 
 set -e
 echo "Executing Terraform Plan on pull request code"
+cd $workspace
 terraform init $tf_init_cli_options
 terraform validate $tf_validation_cli_options
 terraform plan $tf_plan_cli_options
