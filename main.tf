6eba02087f4749e0eae904a70a34d4fa57344923
Nelson Sanchez <anthony69snake@gmail.com>
2021-03-03 16:20:50 -0600

    am

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 provider "aws" {
-  region                  = "us-east-1"
+  region = "us-east-1"
 }
 # build a vpc
 
-resource "aws_vpc" "aws_vpc"{
-    cidr_block  = "10.50.0.0/16"
+resource "aws_vpc" "aws_vpc" {
+  cidr_block = "10.50.0.0/16"
 
-    tags = {
-        Name = "TravisCIvpc"
-    }
+  tags = {
+    Name = "TravisCIvpc"
+  }
 }
 
-resource "aws_subnet" "main"{
-    vpc_id = aws_vpc.aws_vpc.id
-    cidr_block = "10.50.1.0/24"
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.aws_vpc.id
+  cidr_block = "10.50.1.0/24"
 
-    tags = {
-        Name = "TravisCIcreatedSubnet"
-    }
+  tags = {
+    Name = "TravisCIcreatedSubnet"
+  }
 }
