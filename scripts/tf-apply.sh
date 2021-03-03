7b46632fbd919c17cd3fdbe7c08ab966ec16b39a
Nelson Sánchez <45546437+AnthonySnk@users.noreply.github.com>
2021-03-03 15:25:48 -0600

    Add files via upload
    Created from scripts

---
+++ scripts/tf-apply.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+echo "Executing Terraform Apply on merged code"
+terraform init $tf_init_cli_options
+terraform apply $tf_apply_cli_options
---
+++ scripts/tf-plan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+echo "Executing Terraform Plan on pull request code"
+terraform init $tf_init_cli_options
+terraform validate $tf_validation_cli_options
+terraform plan $tf_plan_cli_options
