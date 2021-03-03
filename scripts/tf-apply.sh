a55d0bacba6dcc404e4fb018b9ab3b1c90219edb
Nelson Sanchez <anthony69snake@gmail.com>
2021-03-03 17:19:49 -0600

    am

--- scripts/tf-apply.sh
+++ scripts/tf-apply.sh
@@ -2,8 +2,7 @@
 
 set -e
 echo "Executing Terraform Apply on merged code"
-terraform init "$tf_init_cli_options"
-terraform validate "$tf_validation_cli_options"
-echo $tf_plan_cli_options
-terraform plan "$tf_plan_cli_options"
+terraform init $tf_init_cli_options
+terraform validate $tf_validation_cli_options
+terraform plan $tf_plan_cli_options
 # terraform apply $tf_apply_cli_options
--- scripts/tf-plan.sh
+++ scripts/tf-plan.sh
@@ -2,6 +2,6 @@
 
 set -e
 echo "Executing Terraform Plan on pull request code"
-terraform init "$tf_init_cli_options"
-terraform validate "$tf_validation_cli_options"
-terraform plan "$tf_plan_cli_options"
+terraform init $tf_init_cli_options
+terraform validate $tf_validation_cli_options
+terraform plan "$tf_plan_cli_options
