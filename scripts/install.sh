b228a1380cc2d1c7293f36acd0cdd8bfec83fda7
Nelson Sánchez <45546437+AnthonySnk@users.noreply.github.com>
2021-03-03 15:45:51 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
 set -e
 
-sudo updates -y
-sudo apt install wget
 wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
+
 unzip terraform_"$tf_version"_linux_amd64.zip
+
 sudo mv terraform /usr/local/bin/
+
 rm terraform_"$tf_version"_linux_amd64.zip
