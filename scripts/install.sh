02d2bec1353e93e324148cf272369b1927eadf3a
Nelson Sanchez <anthony69snake@gmail.com>
2021-03-05 15:18:49 -0600

    Hola

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-source scripts/functions_file.sh
+source ./scripts/functions_file.sh
 
 echo "DOWNLOADING TERRAFORM"
 wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
@@ -16,7 +16,7 @@ versionAWScli=$(aws --version)
 rm awscli-exe-linux-x86_64.zip
 echo "AWS CLI VERSION: $versionAWScli"
 
-echo "Exec function file"
+echo "CALLING FUNCTION FILE"
 get_inf
 # DEVELOPER
 # - instalar terraform con version 0.13.6
