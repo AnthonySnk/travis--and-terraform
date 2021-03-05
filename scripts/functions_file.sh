cf7570aa4288bfa011ee5b7d25e840c275a552aa
Nelson Sanchez <anthony69snake@gmail.com>
2021-03-05 15:29:07 -0600

    Hola

--- scripts/functions_file.sh
+++ scripts/functions_file.sh
@@ -4,5 +4,11 @@ set -e
 get_inf(){
     echo "DOWNLOADING VARIABLES"
     aws s3 cp s3://"$bucketName"/"$environment"/terraform.tfvars .
-    mv ./terraform.tfvars ./development
+    
+    if [ $environment -eq "dev" ]; then
+        mv ./terraform.tfvars ./development
+    elif [ $environment -eq "prod" ]
+    then
+        mv ./terraform.tfvars ./production
+    fi
 }
